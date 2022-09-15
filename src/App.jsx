02d0565a2ff452a3e81b94c77b982f608dc02273
Rupesh Khadka <rupesh@alwazebrand.com>
2022-09-15 16:03:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+} from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <>
+    <Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/home" element={<Home />} />
+
+     
+      <Route path="*" element={<p>Page not found</p>} />
+    </Routes>
+  </Router>
+  </>
+  )
+}
+
+export default App
