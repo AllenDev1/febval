41da1e5720c7476c9bf4830fa63c4d4c9fbb643e
Rupesh Khadka <rupesh@alwazebrand.com>
2022-09-19 21:24:06 +0530

    Carousel

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../Components/footer";
 import NavBar from "../Components/Navbar";
+import Carouse from "../Components/Carousel";
 
 const Home = () => {
 	return (
 		<>
 			<NavBar />
+			<Carouse/>
 			<Footer />
 		</>
 	);
