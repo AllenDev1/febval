7744f1f7dfbd75dadf3fcb51ee43eee716657cef
Rupesh Khadka <rupesh@alwazebrand.com>
2022-09-20 14:51:29 +0530

    carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,85 +1,53 @@
 import React from "react";
-import { Card } from "react-bootstrap";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import Carousel from 'react-bootstrap/Carousel';
+import {Container} from 'react-bootstrap'
 import "../Scss/Carousel.scss";
 
 
 const Carouse = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 1
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 1
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 1
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-    };
+    
+
   return (
-    <Carousel
-						className="team-carousel"
-						swipeable={true}
-						draggable={true}
-						showDots={true}
-						responsive={responsive}
-						ssr={true} // means to render carousel on server-side.
-						infinite={true}
-						deviceType={responsive.deviceType}
-						autoPlaySpeed={1}
-						keyBoardControl={true}
-						customTransition="all .5"
-						transitionDuration={500}
-						containerClass="carousel-container"
-						removeArrowOnDeviceType={["tablet", "mobile"]}
-						autoPlay={false}
-						dotListClass="custom-dot-list-style"
-						itemClass="carousel-item-padding-40-px"
-					>
-						<Card>
-							<Card.Img
-								className="team-image"
-								variant="top"
-								src="https://www2.deloitte.com/content/dam/Deloitte/nl/Images/promo_images/deloitte-nl-cm-digital-human-promo.jpg"
-							/>
-							<Card.Body className="team-card-body">
-								<Card.Title className="team-member-name">Card Title</Card.Title>
-								<Card.Text className="team-member-work">Some quick example</Card.Text>
-							</Card.Body>
-						</Card>
-                        <Card>
-							<Card.Img
-								className="team-image"
-								variant="top"
-								src="https://guardian.ng/wp-content/uploads/2022/02/Beautiful-eyes.-Photo-Gansforever-Osman-526x598.jpg"
-							/>
-							<Card.Body className="team-card-body">
-								<Card.Title className="team-member-name">Card Title</Card.Title>
-								<Card.Text className="team-member-work">Some quick example</Card.Text>
-							</Card.Body>
-						</Card>
-                        <Card>
-							<Card.Img
-								className="team-image"
-								variant="top"
-								src="https://www2.deloitte.com/content/dam/Deloitte/nl/Images/promo_images/deloitte-nl-cm-digital-human-promo.jpg"
-							/>
-							<Card.Body className="team-card-body">
-								<Card.Title className="team-member-name">Card Title</Card.Title>
-								<Card.Text className="team-member-work">Some quick example</Card.Text>
-							</Card.Body>
-						</Card>
-                       
-					</Carousel>
+    <>
+	<Container>
+	<Carousel className="carousel-container">
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src="https://guardian.ng/wp-content/uploads/2022/02/Beautiful-eyes.-Photo-Gansforever-Osman-526x598.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src="https://guardian.ng/wp-content/uploads/2022/02/Beautiful-eyes.-Photo-Gansforever-Osman-526x598.jpg"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://guardian.ng/wp-content/uploads/2022/02/Beautiful-eyes.-Photo-Gansforever-Osman-526x598.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+	</Container></>
   );
 };
 
