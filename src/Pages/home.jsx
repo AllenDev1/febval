4aa52e072bf0180b361ac2e90198c633c21122e7
AllenDev1 <dev.allen252@gmail.com>
2022-09-21 12:41:00 +0545

    :sparkle: cards added

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "../Components/footer";
 import NavBar from "../Components/Navbar";
 import Carouse from "../Components/Carousel";
+import Cardsgroup from "../Components/Cardsgroup";
 
 const Home = () => {
 	return (
 		<>
 			<NavBar />
 			<Carouse/>
+			<Cardsgroup />
 			<Footer />
 		</>
 	);
