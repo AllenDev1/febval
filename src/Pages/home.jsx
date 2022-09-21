a804ea1ea57e556eb27ac7ff7d50525bdd38645c
Rupesh Khadka <rupesh@alwazebrand.com>
2022-09-21 17:26:53 +0530

    Banner and Categories added

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Footer from "../Components/footer";
-import NavBar from "../Components/Navbar";
+import NavBar from "../Components/navbar";
 import Carouse from "../Components/Carousel";
 import Cardsgroup from "../Components/Cardsgroup";
+import Sale from "../Components/Sale";
+import Categories from "../Components/Categories"
 
 const Home = () => {
 	return (
 		<>
 			<NavBar />
 			<Carouse/>
+			<Categories/>
 			<Cardsgroup />
+			<Sale/>
 			<Footer />
 		</>
 	);
