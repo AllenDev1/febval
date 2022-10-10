adab2a1e739341aacefb276e72f60ea3f8b3ffac
AllenDev1 <dev.allen252@gmail.com>
2022-10-10 16:28:09 +0545

    updated presonal details modal

--- src/Components/Updatedetails.jsx
+++ src/Components/Updatedetails.jsx
@@ -1,55 +1,66 @@
 import React from "react";
 import Cross from "../Assets/cross.svg";
 import "../Scss/updatedetails.scss";
-import Form from 'react-bootstrap/Form'
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import { Button } from "react-bootstrap";
 
-const Updatedetails = () => {
-  return (
-    <>
-      <div className="update-details">
-        <div className="update-container">
-          <div className="update-header">
-            <text>Personal Details</text>
-            <img src={Cross} alt=""></img>
-          </div>
-          <div className="form-fill">
-            <form>
-              <label>
-                Enter your full name*
-                <br />
-                <input type="text" />
-              </label>
-              <label>
-                Enter Email*
-                <br />
-                <input type="text" />
-              </label>
-              <label>
-                Enter Phone Number*
-                <br />
-                <input type="number" />
-              </label>
-              <label>
-                Enter Address*
-                <br />
-                <input type="text" />
-              </label>
-            </form>
-          </div>
-          <checkbox />
-          <div className="footer-text"> 
-          <Form.Check aria-label="option 1" /><p >
-            You agree to the <a href=""> Terms and Conditions </a>
-          </p>
-          </div>
-         
-          
-            <button>Update</button>
-         
-        </div>
-      </div>
-    </>
-  );
+const Updatedetails = (props) => {
+	return (
+		<>
+			<Modal
+				{...props}
+				size="md"
+				aria-labelledby="contained-modal-title-vcenter"
+				centered
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id="contained-modal-title-vcenter">
+						<text>Personal Details</text>
+					</Modal.Title>
+				</Modal.Header>
+				<div className="update-details">
+					<div className="update-container">
+						
+						<div className="form-fill">
+							<form>
+								<label>
+									Enter your full name*
+									<br />
+									<input type="text" />
+								</label>
+								<label>
+									Enter Email*
+									<br />
+									<input type="text" />
+								</label>
+								<label>
+									Enter Phone Number*
+									<br />
+									<input type="number" />
+								</label>
+								<label>
+									Enter Address*
+									<br />
+									<input type="text" />
+								</label>
+							</form>
+						</div>
+						<checkbox />
+						<div className="footer-text">
+							<Form.Check aria-label="option 1" />
+							<p>
+								You agree to the{" "}
+								<a href=""> Terms and Conditions </a>
+							</p>
+						</div>
+
+						<button>Update</button>
+					</div>
+				</div>
+			</Modal>
+		</>
+	);
 };
 
 export default Updatedetails;
