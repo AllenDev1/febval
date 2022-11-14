e0f87215e432ad871602f8905f5a10271373f7b8
AllenDev1 <dev.allen252@gmail.com>
2022-11-14 16:03:04 +0545

    admin panal

---
+++ Admin/admin/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./component/Sidebar";
+import "./App.css"
+
+function App() {
+	return (
+		<>
+			<Sidebar />
+		</>
+	);
+}
+
+export default App;
