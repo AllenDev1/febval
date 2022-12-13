c9b8dd2208d0cd9bd2af6f3503a6b44c23cedc2f
Rupesh Khadka <78486743+Rupesh-cell@users.noreply.github.com>
2022-12-13 23:12:23 +0530

    general changes

--- Febval/src/App.jsx
+++ Febval/src/App.jsx
@@ -9,6 +9,8 @@ import ProductsByCategory from "./Pages/ProductsByCategory";
 import Events from "./Pages/Events";
 import Terms from "./Pages/Terms";
 import Pagenotfound from "./Pages/Pagenotfound";
+import WhatsAppWidget from "react-whatsapp-chat-widget";
+import "react-whatsapp-chat-widget/index.css";
 const App = () => {
 	return (
 		<>
@@ -34,6 +36,37 @@ const App = () => {
 				</Routes>
 			</Router>
 			<Footer />
+			<WhatsAppWidget
+				phoneNo="9779863684870"
+				position="right"
+				widgetWidth="300px"
+				widgetWidthMobile="260px"
+				autoOpen={true}
+				autoOpenTimer={5000}
+				messageBox={true}
+				messageBoxTxt="Hi Team, is there any related service available ?"
+				iconSize="50"
+				iconColor="white"
+				iconBgColor="#25D366"
+				headerIcon="https://cdn-icons-png.flaticon.com/512/8377/8377198.png"
+				headerIconColor="#25D366"
+				headerTxtColor="black"
+				headerBgColor="#25D366"
+				headerTitle="Rupesh Khadka"
+				headerCaption="Online"
+				bodyBgColor="#bbb"
+				chatPersonName="Support"
+				chatMessage={
+					<>
+						Hi there 👋 <br />
+						<br /> How can I help you?
+					</>
+				}
+				footerBgColor="#999"
+				btnBgColor="#25D366"
+				btnTxtColor="white"
+				btnTxt="Start Chat"
+			/>
 			
 		</>
 	);
