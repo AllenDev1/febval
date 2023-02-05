cc5047ccfe8029692acb305a3a56baf7a4afa4fb
AllenDev1 <dev.allen252@gmail.com>
2023-02-05 12:44:10 +0545

    Update product.model.js

--- api/models/product.model.js
+++ api/models/product.model.js
@@ -24,7 +24,10 @@ const Product = sequelize.define(
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
-		
+		featured: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+		},
 		price: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
